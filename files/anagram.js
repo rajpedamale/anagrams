4b861c899a36f3251ae63e2e17e43d1eecf5097c
gcXJbu <gcXJbu@cyber-dojo.org>
2023-01-27 11:30:37 +0000

    13 ran tests, predicted green, got green

--- files/anagram.js
+++ files/anagram.js
@@ -4,8 +4,12 @@ function reverseString(str) {
     return str.split("").reverse().join("");
 }
 
+function isEmptyWord(word) {
+  return typeof word === 'undefined';
+}
+
 function createAnagrams(word) {
-  const returnArray = typeof word === 'undefined' ? ['']: [word];
+  const returnArray = isEmptyWord(word) ? ['']: [word];
   if (typeof word === 'string' && word.length > 1)
   returnArray.push(reverseString(word));
   return returnArray;
