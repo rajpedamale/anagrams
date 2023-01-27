82bc29d63385ed799bf35a06a5ca2b9d61700709
gcXJbu <gcXJbu@cyber-dojo.org>
2023-01-27 11:26:25 +0000

    11 ran tests, predicted green, got red

--- files/anagram.js
+++ files/anagram.js
@@ -1,7 +1,10 @@
 'use strict';
 
 function createAnagrams(word) {
-  return typeof word === 'undefined' ? ['']: [word];
+  const returnArray = typeof word === 'undefined' ? ['']: [word];
+  if (typeof word === 'string' && word.length > 1)
+  returnArray.push(word.reverse());
+  return returnArray;
 }
 
 module.exports = createAnagrams;
