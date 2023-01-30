1359d07371a8a212301f2ad6f760da3d9e85db4e
gcXJbu <gcXJbu@cyber-dojo.org>
2023-01-30 09:44:51 +0000

    30 ran tests, predicted green, got red

--- files/anagram.test.js
+++ files/anagram.test.js
@@ -10,4 +10,8 @@ describe('Anagrams', () => {
   it('Should return an array of same word for a single letter', () => {
     expect(createAnagrams('a')).toEqual(['a']);
   });
+
+  it('Should return an array of 2 for a 2 lettered word', () => {
+    expect(createAnagrams('ab')).toEqual(['ab', 'ba']);
+  });
 });
