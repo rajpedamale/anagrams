9915756739054384784f5dd9b7dee17deac2a9ab
gcXJbu <gcXJbu@cyber-dojo.org>
2023-01-30 09:42:56 +0000

    27 ran tests, predicted red, got red

--- files/anagram.test.js
+++ files/anagram.test.js
@@ -7,21 +7,5 @@ describe('Anagrams', () => {
     expect(createAnagrams()).toEqual(['']);
   });
 
-  it('Should return an array of same word for a single letter', () => {
-    expect(createAnagrams('a')).toEqual(['a']);
-  });
 
-  it('Should return an array of 2 for a 2 lettered word', () => {
-    expect(createAnagrams('ab')).toEqual(['ab', 'ba']);
-  });
-
-  it('Should return an array of 1 for a 2 lettered word ' +
-     'with repeated characters', () => {
-    expect(createAnagrams('aa')).toEqual(['aa']);
-  });
-
-  it.skip('Should return 6 items for a 3 letter word', () => {
-    const expected = ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'];
-    expect(createAnagrams('abc')).toEqual(expected);
-  });
 });
