d9d527ef7e5efa6326187d01c15e046f143e0c36
gcXJbu <gcXJbu@cyber-dojo.org>
2023-01-30 09:47:15 +0000

    33 ran tests, predicted green, got green

--- files/anagram.test.js
+++ files/anagram.test.js
@@ -20,4 +20,8 @@ describe('Anagrams', () => {
     expect(createAnagrams('aa')).toEqual(['aa']);
   });
 
+  it.skip('Should return 6 items for a 3 letter word', () => {
+    const expected = ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'];
+    expect(createAnagrams('abc')).toEqual(expected);
+  });
 });
